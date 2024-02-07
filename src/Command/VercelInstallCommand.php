19f98fb783154676cc1f0e830ce4b9dc939d4945
Nakato53 <nakato53@gmail.com>
2024-02-07 20:22:33 +0300

    assets

--- src/Command/VercelInstallCommand.php
+++ src/Command/VercelInstallCommand.php
@@ -66,7 +66,11 @@ class VercelInstallCommand extends Command
             "env"=>[
                 "APP_ENV"=>"prod",
             ],
-            "routes"=> [
+            "routes"=> [ 
+                [ 
+                    "src"=> "/(assets)/(.*)$",
+                    "dest"=> "\/api\/assets.php?path=\/$2" 
+                ],
                 [ 
                     "src"=> "/(.*)",
                     "dest"=> "/api/index.php" 
